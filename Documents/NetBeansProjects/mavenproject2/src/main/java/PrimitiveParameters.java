95af541f4b267c7e56eff04efe8600f52751b7d7
praneeth6 <praneethreddy156@gmail.com>
2017-03-01 15:04:45 -0700

    first commit

---
+++ Documents/NetBeansProjects/mavenproject2/src/main/java/PrimitiveParameters.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author praneethreddy
+ */
+public class PrimitiveParameters {
+   
+	        public static void main(String[] args)
+	{	go();
+	}
+	
+	public static void go()
+	{	int x = 3;
+		int y = 2;        
+		System.out.println("In method go. x: " + x + " y: " + y);
+		falseSwap(x,y);
+		System.out.println("in method go. x: " + x + " y: " + y);
+		moreParameters(x,y);
+		System.out.println("in method go. x: " + x + " y: " + y);
+	}
+	
+	public static void falseSwap(int x, int y)
+	{	System.out.println("in method falseSwap. x: " + x + " y: " + y);
+		int temp = x;
+		x = y;
+		y = temp;
+		System.out.println("in method falseSwap. x: " + x + " y: " + y);
+	}
+	
+	public static void moreParameters(int a, int b)
+	{	System.out.println("in method moreParameters. a: " + a + " b: " + b);
+		a = a * b;
+		b = 12;
+		System.out.println("in method moreParameters. a: " + a + " b: " + b);
+		falseSwap(b,a);
+		System.out.println("in method moreParameters. a: " + a + " b: " + b);	
+	}
+}
+
